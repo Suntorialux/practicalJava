047942ae79d88d3a9de84917815b8ab334de8579
suntorialux <suntorialux@gmail.com>
2015-06-29 21:24:29 +0300

    Lesson_3

--- src/Lesson1/HelloWorld.java
+++ src/Lesson1/HelloWorld.java
@@ -2,6 +2,12 @@ package Lesson1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+
+		Integer a=new Integer(5);
+
+
+		System.out.println((double)a);
+
 		System.out.println("Second Commit");
 		System.out.println("Hello World");
 	}
